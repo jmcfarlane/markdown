b2089573dbb903a37b347c792dfe480e4595a0e9
John McFarlane <john@rockfloat.com>
2024-06-05 17:03:35 -0400

    Fix race where #anchors were broken after first load
    - Remove globals (this fixed the race)
    - Renamed a bunch of variables to more accurately reflect the types

--- gomarkdown.go
+++ gomarkdown.go
@@ -18,59 +18,38 @@ import (
 
 const TOC = "[TOC]"
 
-var (
-	highlightStyle *chroma.Style
-	htmlFormatter  *html.Formatter
-	renderer       *mdhtml.Renderer
-	tocRenderer    *mdhtml.Renderer
+var extensions = parser.CommonExtensions |
+	parser.Attributes |
+	parser.AutoHeadingIDs |
+	parser.Mmark |
+	parser.NoEmptyLineBeforeBlock |
+	parser.OrderedListStart |
+	parser.SuperSubscript
 
-	extensions = parser.CommonExtensions |
-		parser.Attributes |
-		parser.AutoHeadingIDs |
-		parser.Mmark |
-		parser.NoEmptyLineBeforeBlock |
-		parser.OrderedListStart |
-		parser.SuperSubscript
-)
-
-func init() {
-	highlightStyle = mustMakeChromaStyle()
-	htmlFormatter = html.New(html.WithClasses(true), html.TabWidth(2))
-	renderer = makeRenderer()
-	tocRenderer = mdhtml.NewRenderer(
-		mdhtml.RendererOptions{Flags: mdhtml.TOC})
-}
-
-func makeRenderer() *mdhtml.Renderer {
+func makeRenderer(formatter *html.Formatter, style *chroma.Style) *mdhtml.Renderer {
 	htmlFlags := mdhtml.RendererOptions{
 		Flags: mdhtml.CommonFlags |
 			mdhtml.Smartypants |
 			mdhtml.SmartypantsDashes |
 			mdhtml.SmartypantsFractions |
 			mdhtml.SmartypantsLatexDashes,
-		RenderNodeHook: myRenderHook,
+		RenderNodeHook: myRenderHook(formatter, style),
 	}
 	return mdhtml.NewRenderer(htmlFlags)
 }
 
 // https://github.com/alecthomas/chroma#supported-languages
-func mustMakeChromaStyle() *chroma.Style {
+func makeChromaStyle() (*chroma.Style, error) {
 	styleName := "monokailight"
-	highlightStyle = styles.Get(styleName)
-	if highlightStyle == nil {
-		panic(fmt.Sprintf("didn't find style '%s'", styleName))
+	style := styles.Get(styleName)
+	if style == nil {
+		return nil, fmt.Errorf("Failed to get style=%s", styleName)
 	}
-	return highlightStyle
-}
-
-func chromaCSS() ([]byte, error) {
-	w := bytes.NewBufferString("")
-	htmlFormatter.WriteCSS(w, highlightStyle)
-	return w.Bytes(), nil
+	return style, nil
 }
 
 // https://github.com/alecthomas/chroma/blob/master/quick/quick.go
-func htmlHighlight(w io.Writer, source, lang, defaultLang string) error {
+func htmlHighlight(w io.Writer, formatter *html.Formatter, style *chroma.Style, source, lang, defaultLang string) error {
 	if lang == "" {
 		lang = defaultLang
 	}
@@ -87,39 +66,52 @@ func htmlHighlight(w io.Writer, source, lang, defaultLang string) error {
 	if err != nil {
 		return err
 	}
-	return htmlFormatter.Format(w, highlightStyle, it)
+	return formatter.Format(w, style, it)
 }
 
 // an actual rendering of Paragraph is more complicated
-func renderCode(w io.Writer, codeBlock *ast.CodeBlock, entering bool) {
+func renderCode(w io.Writer, formatter *html.Formatter, style *chroma.Style, codeBlock *ast.CodeBlock, entering bool) {
 	defaultLang := ""
 	lang := string(codeBlock.Info)
-	htmlHighlight(w, string(codeBlock.Literal), lang, defaultLang)
+	htmlHighlight(w, formatter, style, string(codeBlock.Literal), lang, defaultLang)
 }
 
-func myRenderHook(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
-	if code, ok := node.(*ast.CodeBlock); ok {
-		renderCode(w, code, entering)
-		return ast.GoToNext, true
+func myRenderHook(formatter *html.Formatter, style *chroma.Style) mdhtml.RenderNodeFunc {
+	return func(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
+		if code, ok := node.(*ast.CodeBlock); ok {
+			renderCode(w, formatter, style, code, entering)
+			return ast.GoToNext, true
+		}
+		return ast.GoToNext, false
 	}
-	return ast.GoToNext, false
 }
 
 func renderMarkdown(m []byte) ([]byte, []byte, error) {
-	// Parse document (Note: parser is not thread safe, so create each time)
-	doc := parser.NewWithExtensions(extensions).Parse(m)
+	// Parse markdown (Note: bits are not thread safe, so create each time)
+	myFormatter := html.New(html.WithClasses(true), html.TabWidth(2))
+	myParser := parser.NewWithExtensions(extensions).Parse(m)
 
-	// Render document
-	page := bytes.NewBufferString("")
-	page.Write(markdown.Render(doc, renderer))
+	// Extract chroma style
+	style, err := makeChromaStyle()
+	if err != nil {
+		return nil, nil, err
+	}
 
-	// Generate TOC
-	header := bytes.NewBufferString("")
-	tocRenderer.RenderHeader(header, doc)
+	// Render body
+	body := bytes.NewBufferString("")
+	body.Write(markdown.Render(myParser, makeRenderer(myFormatter, style)))
 
-	// Write the rendered doc with the TOC replaced
-	b := bytes.Replace(page.Bytes(), []byte(TOC), header.Bytes(), 1)
+	// Render CSS
+	css := bytes.NewBufferString("")
+	myFormatter.WriteCSS(css, style)
 
-	css, err := chromaCSS()
-	return css, b, err
+	// Render TOC
+	toc := bytes.NewBufferString("")
+	mdhtml.NewRenderer(mdhtml.RendererOptions{Flags: mdhtml.TOC}).RenderHeader(toc, myParser)
+
+	// Inject the TOC into the body
+	b := bytes.Replace(body.Bytes(), []byte(TOC), toc.Bytes(), 1)
+
+	// Render css and body
+	return css.Bytes(), b, nil
 }
